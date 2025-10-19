cf9fb173f69e64aa54c13bf412f5374136bab11d
Leandro Gardin <leandro.ivp@hotmail.com>
2025-10-19 18:39:49 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import { componentTagger } from "lovable-tagger";
 export default defineConfig(({ mode }) => ({
   server: {
     host: '0.0.0.0',
-    port: 3000,
+    port: 80,
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
